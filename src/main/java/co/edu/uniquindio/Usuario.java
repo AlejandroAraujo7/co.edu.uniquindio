d450c180c3fec6f91d7a68efbf560325e1a09100
Alejandro <jheniera.araujom@uqvirtual.edu.co>
2024-08-20 17:13:55 -0500

    se agrega nueva clase

---
+++ src/main/java/co/edu/uniquindio/Usuario.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio;
+
+public class Usuario {
+    private String nombre;
+    private String cedula;
+    private String email;
+    private String celular;
+
+    public Usuario(String nombre, String cedula, String email, String celular) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.email = email;
+        this.celular = celular;
+    }
+
+}
